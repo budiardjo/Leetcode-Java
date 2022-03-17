f6d875e52d62b8df72204506868e3408cf74d5d3
budiardjo <abrorabyyu9@gmail.com>
2022-03-17 15:47:04 +0800

    adding new problem

--- array/FindAverages.java
+++ array/FindAverages.java
@@ -1,12 +1,14 @@
 import java.util.Arrays;
 
-class coret {
+class FindAverages {
     public double[] findAverages(int k, int[] array) {
         double[] result = new double[array.length - k + 1];
         double windowSum = 0;
         int windowStart = 0;
         for ( int windowEnd = 0; windowEnd < array.length; windowEnd++) {
             windowSum += array[windowEnd];
+            
+            // System.out.println(windowEnd);
 
             if (windowEnd >= k - 1) {
                 result[windowStart] = windowSum / k;
@@ -15,11 +17,12 @@ class coret {
             }
         }
 
+        
         return result;
     }
 
     public static void main(String[] args) {
-        coret c = new coret();
+        FindAverages c = new FindAverages();
         int[] question = {1,3,2,6,-1,4,1,8,2};
         int target = 5;
         double[] result = c.findAverages(target, question);
